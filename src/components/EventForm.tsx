df64630d33f247099774b9d00013240b9aea9594
alex <alexnbui@gmail.com>
2023-07-20 03:46:07 -0700

    Added cancel buttons to forms

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,5 +1,13 @@
+"use client"
+import { useQuery, useMutation } from "@tanstack/react-query"
+import axios from "axios"
+
 type Props = {
     change: Function
+}
+
+function createEvent(e: any) {
+    alert(e.target.title.value)
 }
 
 function defaultStartTime() {
@@ -17,24 +25,27 @@ function defaultEndTime() {
 export default function EventForm({ change }: Props) {
     return (
         <div className="self-center w-4/5">
-            <form onSubmit={() => change(0)} className="flex flex-col gap-5 w-full">
+            <form onSubmit={() => { createEvent(event); change(0) }} className="flex flex-col gap-5 w-full">
                 <div>
                     <div className="text-right">title</div>
-                    <input type="text" id="title" className="w-full" />
+                    <input type="text" id="title" className="w-full p-1 rounded-md" />
                 </div>
                 <div>
                     <div className="text-right">start time</div>
-                    <input type="datetime-local" id="startTime" className="w-full" value={defaultStartTime()} />
+                    <input type="datetime-local" id="startTime" className="w-full p-1 rounded-md" value={defaultStartTime()} />
                 </div>
                 <div>
                     <div className="text-right">end time</div>
-                    <input type="datetime-local" id="endTime" className="w-full" value={defaultEndTime()} />
+                    <input type="datetime-local" id="endTime" className="w-full p-1 rounded-md" value={defaultEndTime()} />
                 </div>
                 <div>
                     <div className="text-right">notes</div>
-                    <textarea id="notes" className="w-full"></textarea>
+                    <textarea id="notes" className="w-full p-1 rounded-md"></textarea>
                 </div>
-                <input type="submit" className="ui-button" />
+                <div className="flex flex-row gap-3">
+                    <input type="submit" className="ui-button flex-1 bg-blue-200" />
+                    <button className="ui-button flex-1" onClick={() => change(0)}>Cancel</button>
+                </div>
             </form>
         </div>
     )
